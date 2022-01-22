a5d843183ef3c0ce27b9993f02890135275b1e4e
James <he.is.james7@gmail.com>
2022-01-21 19:33:15 -0800

    added more prop fields

--- src/Marketplace/ProduceCard.js
+++ src/Marketplace/ProduceCard.js
@@ -1,20 +1,28 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Text } from 'react-native-paper';
 
-function ProduceCard({ name, price }) {
+function ProduceCard({
+  image, name, price, onPress,
+}) {
   return (
-    <View>
-      <Text>{name}</Text>
-      <Text>{price}</Text>
-    </View>
+    <TouchableOpacity onPress={onPress}>
+      <Image source={image} />
+      <View>
+        <Text>{name}</Text>
+        <Text>{price}</Text>
+      </View>
+    </TouchableOpacity>
   );
 }
 
 ProduceCard.propTypes = {
+  image: PropTypes.func.isRequired,
   name: PropTypes.string.isRequired,
   price: PropTypes.string.isRequired,
+  onPress: PropTypes.func.isRequired,
 };
 
 export default ProduceCard;
