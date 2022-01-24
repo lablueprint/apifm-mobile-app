4abb26286739f08d5d524db578fc820d8a0d4c61
ashi72 <32313598+ashi72@users.noreply.github.com>
2022-01-24 14:43:30 -0800

    created a profile screen to filter by email, currently using hardcoded email instead of by prop or by userdata

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { AppRegistry } from 'react-native';
+import Config from 'react-native-config';
 import App from './src/App';
 import { name as appName } from './app.json';
 
+console.log(Config.REACT_APP_AIRTABLE_BASE_KEY);
 AppRegistry.registerComponent(appName, () => App);
