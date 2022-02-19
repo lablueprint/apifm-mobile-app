fb6aa09e9710ae69d642ec9e8997912f94c60289
James He <62224382+he-is-james@users.noreply.github.com>
2022-02-18 22:01:51 -0500

    marketplace produce card (#8)
    * initial creation of produce card
    
    allowed pass through of components for name and price, will work on getting test img
    
    * added more prop fields
    
    * working navigation of card to details
    
    * props passed from card to detail screen
    
    need to figure out prop types because of eslint error, still builds though
    
    * started styling produce card
    
    need to add icon and figure out flex to position everything
    
    * started airtable integration
    
    can call the table to get produce, will need a way to map the data in produce cards
    
    * display airtable data as produce cards
    
    needs to fix initial load with useState delay
    
    * fixed initial render and styled produce cards
    
    going to marketplace now loads all the produce from the airtable
    styled the produce cards to look like the figma (may need to adjust sizing because they are pixels not percentages)
    
    * fixed spelling error
    
    * adjustments to design and render
    
    adjusted checks when pulling from airtable
    adjusted design of the produce card
    implemented heart like system
    
    * pass in correct images and proptypes

---
+++ src/Marketplace/ProduceDetailsScreen.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { View, Image, StyleSheet } from 'react-native';
+import { Text } from 'react-native-paper';
+
+const styles = StyleSheet.create({
+  image: {
+    width: 205,
+    height: 386,
+  },
+});
+
+function ProduceDetailsScreen({ route }) {
+  const { image, name, price } = route.params;
+
+  const imageurl = { uri: image };
+
+  return (
+    <View>
+      <Text>Produce Details</Text>
+      <Image style={styles.image} source={imageurl} />
+      <Text>{name}</Text>
+      <Text>{price}</Text>
+    </View>
+  );
+}
+
+ProduceDetailsScreen.propTypes = {
+  route: PropTypes.shape({
+    params: PropTypes.shape({
+      image: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+    }),
+  }).isRequired,
+};
+
+export default ProduceDetailsScreen;
