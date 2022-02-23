8319d43d59b7bdc925f5e03debbbd680864271b2
Grace Ma <glm1550@ucla.edu>
2022-02-23 13:45:12 -0800

    sign in screen functionality added. no organization name on delivery page - will check in with design team

--- src/SignIn/SignInScreen.js
+++ src/SignIn/SignInScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, setState, useRef } from 'react';
 import {
   View, StyleSheet, TextInput, Alert,
 } from 'react-native';
@@ -9,6 +9,9 @@ import Config from 'react-native-config';
 import PropTypes from 'prop-types';
 
 import Icon from 'react-native-vector-icons/FontAwesome5';
+import symbolicateStackTrace from 'react-native/Libraries/Core/Devtools/symbolicateStackTrace';
+import { State } from 'react-native-gesture-handler';
+// import SignInScreen2 from './SignInScreen2';
 
 const Airtable = require('airtable');
 
@@ -16,7 +19,7 @@ const airtableConfig = {
   apiKey: Config.REACT_APP_AIRTABLE_USER_KEY,
   baseKey: Config.REACT_APP_AIRTABLE_BASE_KEY,
 };
-
+// {boolean ? <> : <>}
 const base = new Airtable({ apiKey: airtableConfig.apiKey })
   .base(airtableConfig.baseKey);
 
@@ -41,6 +44,7 @@ const styles = StyleSheet.create({
 });
 
 export default function SignInScreen({ navigation }) {
+  // first page states:
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [organization, setOrganization] = useState('');
@@ -60,137 +64,252 @@ export default function SignInScreen({ navigation }) {
   const numberInput = useRef();
   const busPhoneInput = useRef();
 
-  const handleSignUp = () => {
+  // second page states:
+  const [address, setAddress] = useState('');
+  const [apt, setApt] = useState('');
+  const [zip, setZip] = useState('');
+  const [instr, setInstr] = useState('');
+
+  const aptInput = useRef();
+  const zipInput = useRef();
+  const instrInput = useRef();
+
+  const [page1, setPage1] = useState(true);
+
+  const checkInputs = () => {
     if (firstName === '') {
       Alert.alert('Please enter the First Name to proceed');
-      // return 1;
     } else if (lastName === '') {
       Alert.alert('Please enter the Last Name to proceed');
-      // return 1;
     } else if (email === '') {
       Alert.alert('Please enter the Email to proceed');
-      // return 1;
     } else if (password === '') {
       Alert.alert('Please enter the Password to proceed');
-      // return 1;
     } else if (number === '') {
       Alert.alert('Please enter the Personal phone number to proceed');
-      // return 1;
     } else if (password !== confirmPassword) {
       Alert.alert('Password Confirmation does not match Password');
     } else {
-      navigation.navigate('SignIn2');
+      return true;
     }
-    // return 0;
+    // else {
+    //   navigation.navigate('SignIn2');
+    // }
   };
 
+  const handleSignUp = () => {
+    if (address === '') {
+      Alert.alert('Please enter the Address to proceed');
+    } else if (zip === '') {
+      Alert.alert('Please enter the Zipcode to proceed');
+    } else {
+      base('Users').create([
+        {
+          fields: {
+            email,
+            password,
+            'first name': firstName,
+            'last name': lastName,
+            organization,
+            'personal phone': number,
+            'business phone': busPhone,
+            address,
+            apt,
+            zip,
+            instr,
+          },
+        },
+      ], (err) => {
+        if (err) {
+          Alert.alert(err, err.message);
+        }
+      });
+      setFirstName('');
+      setLastName('');
+      setOrganization('');
+      setEmail('');
+      setPassword('');
+      setNumber('');
+      setBusPhone('');
+      setAddress('');
+      setApt('');
+      setZip('');
+      setInstr('');
+      navigation.navigate('SignIn2');
+    }
+  };
+
+  if (page1) {
+    return (
+      <View style={styles.container}>
+
+        <Title style={styles.titleText}>Sign Up</Title>
+
+        <TextInput
+          value={firstName}
+          onChangeText={setFirstName}
+          placeholder="First name"
+          returnKeyType="next"
+          onSubmitEditing={() => { lastNameInput.current.focus(); }}
+          blurOnSubmit={false}
+        />
+        <TextInput
+          value={lastName}
+          onChangeText={setLastName}
+          placeholder="Last name"
+          returnKeyType="next"
+          onSubmitEditing={() => { organizationInput.current.focus(); }}
+          blurOnSubmit={false}
+          ref={lastNameInput}
+        />
+        <TextInput
+          value={organization}
+          onChangeText={setOrganization}
+          placeholder="Organization (optional)"
+          returnKeyType="next"
+          onSubmitEditing={() => { emailInput.current.focus(); }}
+          blurOnSubmit={false}
+          ref={organizationInput}
+        />
+        <TextInput
+          value={email}
+          onChangeText={setEmail}
+          placeholder="Email address"
+          keyboardType="email-address"
+          returnKeyType="next"
+          onSubmitEditing={() => { passwordInput.current.focus(); }}
+          blurOnSubmit={false}
+          ref={emailInput}
+        />
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <TextInput
+            value={password}
+            onChangeText={setPassword}
+            placeholder="Password"
+            returnKeyType="next"
+            secureTextEntry={!!hidePass}
+            onSubmitEditing={() => { confirmPasswordInput.current.focus(); }}
+            blurOnSubmit={false}
+            ref={passwordInput}
+          />
+          <Icon
+            name={hidePass ? 'eye-slash' : 'eye'}
+            size={15}
+            color="grey"
+            onPress={() => setHidePass(!hidePass)}
+          />
+        </View>
+
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <TextInput
+            value={confirmPassword}
+            onChangeText={setConfirmPassword}
+            placeholder="Confirm Password"
+            returnKeyType="next"
+            secureTextEntry={!!hidePassConf}
+            onSubmitEditing={() => { numberInput.current.focus(); }}
+            blurOnSubmit={false}
+            ref={confirmPasswordInput}
+          />
+          <Icon
+            name={hidePassConf ? 'eye-slash' : 'eye'}
+            size={15}
+            color="grey"
+            onPress={() => setHidePassConf(!hidePassConf)}
+          />
+        </View>
+
+        <TextInput
+          value={number}
+          onChangeText={setNumber}
+          placeholder="Personal phone number"
+          keyboardType="numeric"
+          returnKeyType="next"
+          onSubmitEditing={() => { busPhoneInput.current.focus(); }}
+          blurOnSubmit={false}
+          ref={numberInput}
+        />
+
+        <TextInput
+          value={busPhone}
+          onChangeText={setBusPhone}
+          placeholder="Business phone number (optional)"
+          keyboardType="numeric"
+          returnKeyType="next"
+          blurOnSubmit={false}
+          ref={busPhoneInput}
+        />
+
+        {/* <SignInScreen2 data={firstName.data} /> */}
+        <Button
+          mode="contained"
+          style={styles.button}
+          onPress={() => {
+            if (checkInputs()) {
+              setPage1(!page1);
+            }
+          }}
+        >
+          Continue
+        </Button>
+
+      </View>
+    );
+  }
+
+  // second page:
   return (
     <View style={styles.container}>
 
-      <Title style={styles.titleText}>Sign Up Form</Title>
-      <TextInput
-        value={firstName}
-        onChangeText={setFirstName}
-        placeholder="First name"
-        returnKeyType="next"
-        onSubmitEditing={() => { lastNameInput.current.focus(); }}
-        blurOnSubmit={false}
-      />
-      <TextInput
-        value={lastName}
-        onChangeText={setLastName}
-        placeholder="Last name"
-        returnKeyType="next"
-        onSubmitEditing={() => { organizationInput.current.focus(); }}
-        blurOnSubmit={false}
-        ref={lastNameInput}
-      />
-      <TextInput
-        value={organization}
-        onChangeText={setOrganization}
-        placeholder="Organization (optional)"
-        returnKeyType="next"
-        onSubmitEditing={() => { emailInput.current.focus(); }}
-        blurOnSubmit={false}
-        ref={organizationInput}
-      />
-      <TextInput
-        value={email}
-        onChangeText={setEmail}
-        placeholder="Email address"
-        keyboardType="email-address"
-        returnKeyType="next"
-        onSubmitEditing={() => { passwordInput.current.focus(); }}
-        blurOnSubmit={false}
-        ref={emailInput}
-      />
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <TextInput
-          value={password}
-          onChangeText={setPassword}
-          placeholder="Password"
-          returnKeyType="next"
-          secureTextEntry={!!hidePass}
-          onSubmitEditing={() => { confirmPasswordInput.current.focus(); }}
-          blurOnSubmit={false}
-          ref={passwordInput}
-        />
-        <Icon
-          name={hidePass ? 'eye-slash' : 'eye'}
-          size={15}
-          color="grey"
-          onPress={() => setHidePass(!hidePass)}
-        />
-      </View>
+      <Title style={styles.titleText}>Sign Up</Title>
 
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <TextInput
-          value={confirmPassword}
-          onChangeText={setConfirmPassword}
-          placeholder="Confirm Password"
-          returnKeyType="next"
-          secureTextEntry={!!hidePassConf}
-          onSubmitEditing={() => { numberInput.current.focus(); }}
-          blurOnSubmit={false}
-          ref={confirmPasswordInput}
-        />
-        <Icon
-          name={hidePassConf ? 'eye-slash' : 'eye'}
-          size={15}
-          color="grey"
-          onPress={() => setHidePassConf(!hidePassConf)}
-        />
-      </View>
+      <Text>Delivery Address</Text>
 
       <TextInput
-        value={number}
-        onChangeText={setNumber}
-        placeholder="Personal phone number"
-        keyboardType="numeric"
+        value={address}
+        onChangeText={setAddress}
+        placeholder="Street address"
         returnKeyType="next"
-        onSubmitEditing={() => { busPhoneInput.current.focus(); }}
+        onSubmitEditing={() => { aptInput.current.focus(); }}
         blurOnSubmit={false}
-        ref={numberInput}
+      />
+      <TextInput
+        value={apt}
+        onChangeText={setApt}
+        placeholder="Apt # (optional)"
+        returnKeyType="next"
+        onSubmitEditing={() => { zipInput.current.focus(); }}
+        blurOnSubmit={false}
+        ref={aptInput}
+      />
+      <TextInput
+        value={zip}
+        onChangeText={setZip}
+        placeholder="Zip Code"
+        returnKeyType="next"
+        onSubmitEditing={() => { instrInput.current.focus(); }}
+        blurOnSubmit={false}
+        ref={zipInput}
+      />
+      <TextInput
+        value={instr}
+        onChangeText={setInstr}
+        placeholder="Instructions for delivery (optional)"
+        returnKeyType="next"
+        blurOnSubmit={false}
+        ref={instrInput}
       />
 
-      <TextInput
-        value={busPhone}
-        onChangeText={setBusPhone}
-        placeholder="Business phone number (optional)"
-        keyboardType="numeric"
-        returnKeyType="next"
-        blurOnSubmit={false}
-        ref={busPhoneInput}
-      />
+      <Text>
+        The address will be used for delivery and to calculate the order minimum for delivery.
+      </Text>
 
       <Button
         mode="contained"
         style={styles.button}
-        onPress={() => {
-          handleSignUp();
-        }}
+        onPress={handleSignUp}
       >
-        Continue
+        Sign Up
       </Button>
 
     </View>
--- src/SignIn/SignInScreen2.js
+++ src/SignIn/SignInScreen2.js
@@ -8,18 +8,6 @@ import {
 import Config from 'react-native-config';
 import PropTypes from 'prop-types';
 
-import Icon from 'react-native-vector-icons/FontAwesome5';
-
-const Airtable = require('airtable');
-
-const airtableConfig = {
-  apiKey: Config.REACT_APP_AIRTABLE_USER_KEY,
-  baseKey: Config.REACT_APP_AIRTABLE_BASE_KEY,
-};
-
-const base = new Airtable({ apiKey: airtableConfig.apiKey })
-  .base(airtableConfig.baseKey);
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -41,90 +29,14 @@ const styles = StyleSheet.create({
 });
 
 export default function SignInScreen({ navigation }) {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [organization, setOrganization] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [number, setNumber] = useState('');
-  const [busPhone, setBusPhone] = useState('');
-  const [address, setAddress] = useState('');
-  const [hidePass, setHidePass] = useState(true);
-
-  const lastNameInput = useRef();
-  const organizationInput = useRef();
-  const emailInput = useRef();
-  const passwordInput = useRef();
-  const numberInput = useRef();
-  const busPhoneInput = useRef();
-  const addressInput = useRef();
-
-  const handleSignUp = () => {
-    if (firstName === '') {
-      Alert.alert('Please enter the First Name to proceed');
-    } else if (lastName === '') {
-      Alert.alert('Please enter the Last Name to proceed');
-    } else if (email === '') {
-      Alert.alert('Please enter the Email to proceed');
-    } else if (password === '') {
-      Alert.alert('Please enter the Password to proceed');
-    } else if (number === '') {
-      Alert.alert('Please enter the Personal phone number to proceed');
-    } else {
-      base('Users').create([
-        {
-          fields: {
-            email,
-            password,
-            'first name': firstName,
-            'last name': lastName,
-            organization,
-            'personal phone': number,
-            'business phone': busPhone,
-            address,
-          },
-        },
-      ], (err) => {
-        if (err) {
-          // Alert.alert(err);
-          Alert.alert(err, err.message);
-        }
-      });
-      setFirstName('');
-      setLastName('');
-      setOrganization('');
-      setEmail('');
-      setPassword('');
-      setNumber('');
-      setBusPhone('');
-      // setBusPhone('');
-      setAddress('');
-    }
-  };
-
   return (
     <View style={styles.container}>
+      <Text>Thank you for signing up with Food Roots! Our staff will</Text>
+      <Text> review your information, and your account should</Text>
+      <Text> be approved within 1-3 business days.</Text>
 
-      <Title style={styles.titleText}>Sign Up Form</Title>
-
-      <TextInput
-        value={address}
-        onChangeText={setAddress}
-        placeholder="Street address"
-        ref={addressInput}
-      />
-      <Button
-        mode="contained"
-        style={styles.button}
-        onPress={handleSignUp}
-      >
-        Sign Up
-      </Button>
+      <Button> Start ordering!</Button>
 
     </View>
   );
 }
-
-SignInScreen.propTypes = {
-  navigation: PropTypes.shape({ navigate: PropTypes.func }).isRequired,
-};
