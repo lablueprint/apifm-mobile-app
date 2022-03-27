9cd753cf53e29991c05b11c9cef3f74614d71593
Kaylee Tran <kayleetran@s-128-97-104-148.resnet.ucla.edu>
2022-03-27 16:16:16 -0700

    cartProduct pls be there

--- src/Cart/CartScreen.js
+++ src/Cart/CartScreen.js
@@ -1,41 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, ScrollView } from 'react-native';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import {
-  Title, Button,
+  Title, Text, Button,
 } from 'react-native-paper';
-import { PropTypes } from 'prop-types';
-import Config from 'react-native-config';
+import PropTypes from 'prop-types';
 
-import CartProduct from './CartProduct';
-
-// airtable
-const Airtable = require('airtable');
-
-const airtableConfig = {
-  apiKey: Config.REACT_APP_AIRTABLE_USER_KEY,
-  baseKey: Config.REACT_APP_AIRTABLE_BASE_KEY,
-};
-
-const base = new Airtable({ apiKey: airtableConfig.apiKey })
-  .base(airtableConfig.baseKey);
-
-// style sheet
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: '-35%',
-  },
-  productsContainer: {
-    flex: 1,
-    // justifyContent: 'start',
-    alignItems: 'center',
-    marginTop: '5%',
   },
   titleText: {
-    marginBottom: '2%',
-    marginTop: '40%',
+    marginBottom: 10,
   },
   bodyText: {
     marginLeft: 5,
@@ -46,83 +23,15 @@ const styles = StyleSheet.create({
     marginTop: 10,
     backgroundColor: '#0492c2',
   },
-  scrollView: {
-    flex: 1,
-    width: '100%',
-    marginBottom: 10,
-  },
 });
 
-// const fakeItems = { name: 'daikon', quantity: '2' };
-
 export default function CartScreen({ navigation }) {
-  const [itemList, setItemList] = useState([]);
-  const [refresh, setRefresh] = useState(0);
-
-  const getItems = () => {
-    const list = [];
-    base('Cart').select({}).eachPage((records, fetchNextPage) => {
-      records.forEach((record) => {
-        const item = record;
-        console.log(record.fields);
-        list.push(item.fields);
-      });
-      setItemList(list);
-      fetchNextPage();
-    });
-  };
-
-  // LEFT TO DO LATER BECAUSE RIGHT NOW IT DOESNT WORK
-  // const handleCheckout = (itemList) => {
-  //   forEach (something) => {
-  //     base('Cart').update([
-  //       {
-  //         id: item.item_id,
-  //         fields: {
-  //           checkout: 'checked out',
-  //         },
-  //       },
-  //     ], (err, records) => {
-  //       if (err) {
-  //         console.error(err);
-  //         return;
-  //       }
-  //       records.forEach((record) => {
-  //         console.log(record.get('type'));
-  //       });
-  //     });
-  //   }
-  // };
-
-  const products = itemList.map((item) => (
-    <CartProduct
-      itemID={item.item_id}
-      key={item.item_id}
-      setRefresh={setRefresh}
-      refresh={refresh}
-    />
-  ));
-
-  useEffect(() => {
-    getItems();
-  }, [refresh]);
-
   return (
     <View style={styles.container}>
       <Title style={styles.titleText}>
-        best cart ever omg who made this she must be hot
+        Cart
       </Title>
-      {/* probably need this later <Text style={styles.bodyext}> */}
-      <ScrollView style={styles.scrollView}>
-        {products}
-
-      </ScrollView>
-      <Button
-        mode="contained"
-        onPress={console.log('pressed')}
-      >
-        CHECKOUT
-      </Button>
+      <Text style={styles.bodyText}> I am a shopping cart. </Text>
       <Button
         mode="contained"
         style={styles.button}
