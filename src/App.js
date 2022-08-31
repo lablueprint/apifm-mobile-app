0b5bc221ea1276c3e9ced8629d8cf901a357afe4
James <he.is.james7@gmail.com>
2022-08-31 14:12:08 -0400

    fixed small changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import store from './lib/redux/store';
 import AppNavigationContainer from './Navigation';
 
 export default function App() {
   return (
-    <View style={{ flex: 1 }}>
-      <AppNavigationContainer />
-    </View>
+    <Provider store={store}>
+      <View style={{ flex: 1 }}>
+        <AppNavigationContainer />
+      </View>
+    </Provider>
   );
 }
