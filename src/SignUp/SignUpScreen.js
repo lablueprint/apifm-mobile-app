21b792257aea4336afdbec3bc9620c2871014bc2
James <he.is.james7@gmail.com>
2022-09-14 16:48:22 -0400

    remove accidental import

--- src/SignUp/SignUpScreen.js
+++ src/SignUp/SignUpScreen.js
@@ -11,7 +11,6 @@ import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import ArrowIcon from 'react-native-vector-icons/AntDesign';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
-import { add, set } from 'lodash';
 import { registerUser } from '../lib/airlock/airlock';
 
 const foodrootslogo = require('../assets/imgs/foodrootsharvest.png');
